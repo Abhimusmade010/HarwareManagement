c566caf634fbad6a0516f562ea96a9b4f9c59f8f
Abhimusmade010 <abhishekmusmade342@gmail.com>
2026-07-06 17:23:16 +0530

    By pass the mailing

--- src/utils/sendEmail.js
+++ src/utils/sendEmail.js
@@ -10,14 +10,9 @@ const transporter = nodemailer.createTransport({
 });
 
 const sendEmail = async ({to,subject,html}) => {
-
-    await transporter.sendMail({
-        from: process.env.EMAIL_USER,
-        to,
-        subject,
-        html
-    });
-
+    // Bypassing email sending to prevent Render SMTP timeout errors
+    console.log(`[EMAIL BYPASSED] Would have sent email to: ${to} with subject: ${subject}`);
+    return Promise.resolve();
 };
 
 export default sendEmail;
